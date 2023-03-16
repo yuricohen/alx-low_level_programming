54d2f56a5e141aecf7c4447f08514a36c56ae2ba
Yuri cohen <alazizielmehdi@gmail.com>
2023-03-16 15:28:18 -0700

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - defines start of the program
+* * Return:0
+* */
+int main(void) 
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
