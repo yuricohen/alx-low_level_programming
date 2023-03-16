2a8f5e6b954a20f5952cb7df965d25d70cd646d9
Yuri cohen <alazizielmehdi@gmail.com>
2023-03-16 15:23:31 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - defines start of the program 
- * Return:0
-*/
+* main - defines start of the program
+* * Return:0
+* */
 int main(void) 
 {
 puts("\"Programming is like building a multilingual puzzle");
