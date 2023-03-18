25194b81ae49b845fa779c9232aadd9e81fe17e4
Yuri cohen <alazizielmehdi@gmail.com>
2023-03-18 03:58:11 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,15 +18,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n>0)
 {
-	printf("%d\n is positive\n");
+	printf("%d is positive\n", n);
 }
 	else if (n == 0 )
 {
-	printf("%d\n is 0\n");
+	printf("%d is is zero\n", n);
 }
 	else
 {
-	printf("%d\n is negative\n");
+	printf("%d is negative\n", n);
 }
 	return (0);
 }
