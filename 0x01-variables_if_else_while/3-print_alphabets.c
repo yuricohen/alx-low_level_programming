39ff421bda82d376a8267b234fa932496fec8c7d
Yuri cohen <alazizielmehdi@gmail.com>
2023-03-19 05:25:26 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - defines start of the program
+*
+* n - defines the variable that will be generated
+*
+* Return:0
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+{
+	putchar(c);
+}
+	for (c = 'A'; c <= 'Z'; c++)
+{	
+	putchar(c);
+}
+	putchar('\n');
+	return (0);
+}
