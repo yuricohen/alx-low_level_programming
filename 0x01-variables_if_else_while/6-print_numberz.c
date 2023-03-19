0502b280c7e274abf70aca3dbcbfb6230c8f871c
Yuri cohen <alazizielmehdi@gmail.com>
2023-03-19 14:27:38 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - defines start of the program
+*
+* n - defines the variable that will be generated
+*
+* Return:0
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+{
+	putchar(i + "0");
+}
+	putchar("\n");
+	return (0);
+}
