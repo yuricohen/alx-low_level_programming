39eef1222ffedd66dcb25c82dfcc0ec391809e28
Yuri cohen <alazizielmehdi@gmail.com>
2023-03-19 14:54:43 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,12 +11,16 @@
 int main(void)
 {
 	int n;
+	char c;
 
 	for (n = 0; n < 10; n++)
 {
 	putchar(n + '0');
 }
-
+	for (c = 'a'; c <= 'f'; c++)
+{
+	putchar(c);
+}
 	putchar('\n');
 	return (0);
 }
