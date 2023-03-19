c3150419a39313975c7f1e432ed636eb47aa004f
Yuri cohen <alazizielmehdi@gmail.com>
2023-03-19 13:14:04 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - defines start of the program
+*
+* n - defines the variable that will be generated
+*
+* Return:0
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	if (c != 'e' && c != 'q')
+{
+	putchar(c) | grep [ eq ];
+}
+	putchar('\n');
+	return (0);
+}
