970349c4d0b4b462c8ef3cc7b15ad87b16ce3032
Yuri cohen <alazizielmehdi@gmail.com>
2023-03-21 16:12:43 -0700

    upgradeoftask

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,20 +1,4 @@
 #include <stdio.h>
 
 void printing(void);
-
-void printing(void)
-
-{
-    printf("_putchar\n");
-}
-
-void print_alphabet(void)
-{
-    char letter = 'a';
-    while (letter <= 'z')
-    {
-        putchar(letter);
-        letter++;
-    }
-    putchar('\n');
-}
+void print_alphabet(void);
