343ff2c94a725ef4cc14c68130ac61dbde937b9c
Yuri cohen <alazizielmehdi@gmail.com>
2023-03-21 15:49:27 -0700

    task1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,14 @@ void printing(void)
 {
     printf("_putchar\n");
 }
+
+void print_alphabet(void)
+{
+    char letter = 'a';
+    while (letter <= 'z')
+    {
+        _putchar(letter);
+        letter++;
+    }
+    _putchar('\n');
+}
