95117697114058707c803aff539d93cbac9a604b
Yuri cohen <alazizielmehdi@gmail.com>
2023-03-21 15:15:15 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
 * main - defines start of the program
+*
+* task1 - is the sting to be printed
 *
 * Return:0
 */
 
+char *task1 ="_putchar"
 int main(void)
 {
-	printf("_putchar\n");
+	printing();
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+// Declare the function prototype
+void printing(void);
+
+// Implement the function
+void printing(void)
+
+{
+    printf(""%s\n",task1);
+}
