0f8d4d0b540f21af17d8de4ca1442094545ab917
Yuri cohen <alazizielmehdi@gmail.com>
2023-03-21 15:35:26 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - defines start of the program
+*
+* Return:0
+*/
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,9 @@ void printing(void)
 {
     printf("_putchar\n");
 }
+
+void print_alphabet(void)
+{
+char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	printf("%s\n", alphabet);
+}
