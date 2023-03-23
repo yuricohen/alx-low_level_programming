6022b9a028778b014b2eebffc4d57c08cb4ffc44
Yuri cohen <alazizielmehdi@gmail.com>
2023-03-23 16:15:57 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,24 @@
-#include <stdio.h>
 #include "main.h"
 #include <unistd.h>
+
 /**
-* main - defined the beginnig of the program
-* printing - defines start of the program
-*
-* Return:0
-*/
-
+ * printing - prints the string _putchar, followed by a new line
+ */
 void printing(void)
-
 {
-	char s[] = "_putchar\n";
-	int i;
+        char s[] = "_putchar\n";
+        int i;
 
-	for (i = 0; s[i] != '\0'; i++)
-	_putchar(s[i]); 
+        for (i = 0; s[i] != '\0'; i++)
+            _putchar(s[i]);
+}
+
+/**
+ * main - entry point of the program
+ * Return: 0
+ */
+int main(void)
+{
+        printing();
+        return (0);
 }
